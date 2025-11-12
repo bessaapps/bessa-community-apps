1bb9c48f2d772701bbb2e312e06eea3ee74cb85e
topher <topherjamesknoll@gmail.com>
2025-11-11 16:48:41 -0800

    Think twice. Code once.

--- components/SectionHeading.tsx
+++ components/SectionHeading.tsx
@@ -2,9 +2,8 @@ import { ReactNode } from "react";
 
 export default function SectionHeading({ children }: { children: ReactNode }) {
   return (
-    <>
-      <h2 className={"text-accent-foreground font-bold mb-4"}>{children}</h2>
-      <div className={"divider"}></div>
-    </>
+    <h2 className={"text-accent-foreground text-2xl font-bold mb-4"}>
+      {children}
+    </h2>
   );
 }
