740ac17b518eaffb3c215b13ddcaf97293e41345
topher <topherjamesknoll@gmail.com>
2025-11-19 18:50:09 -0800

    Think twice. Code once.

--- components/Weather.tsx
+++ components/Weather.tsx
@@ -9,22 +9,25 @@ export default function Weather() {
 
   useEffect(() => {
     axios
-      .get("https://api.weather.gov/gridpoints/VEF/123,97/forecast/hourly")
+      .get("https://api.weather.gov/gridpoints/VEF/123,97/forecast/hourly", {
+        headers: {
+          "User-Agent": "BessaCommunityApps (topher@bessaapps.com)",
+          Accept: "application/ld+json"
+        }
+      })
       .then((response) => {
         console.log(response.data);
-        setShortForecast(
-          response.data?.properties?.periods?.[0]?.shortForecast
-        );
-        setIsDaytime(response.data?.properties?.periods?.[0]?.isDaytime);
+        setShortForecast(response.data?.periods?.[0]?.shortForecast);
+        setIsDaytime(response.data?.periods?.[0]?.isDaytime);
       })
       .catch((error) => console.error(error));
   }, []);
 
   return (
-    <p className={"text-xs text-center fixed top-0 left-[50%]"}>
+    <p className={"text-xs fixed top-0 left-[50%]"}>
       Las Vegas, NV&nbsp;&nbsp;|&nbsp;&nbsp;36.155279 N. 115.113745 W.
       <br />
-      {shortForecast}
+      {isDaytime ? "☀︎" : "⏾"} {shortForecast}
     </p>
   );
 }
