ae67e5045ffc830985a2022a40524aef4f0d01a5
topher <topherjamesknoll@gmail.com>
2025-11-21 19:15:36 -0800

    Think twice. Code once.

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,21 @@ const nextConfig: NextConfig = {
   async redirects() {
     return [
       {
+        source: "/services/custom-mobile-app-development",
+        destination:
+          "/services/custom-mobile-app-development-that-drives-growth",
+        permanent: true
+      },
+      {
+        source: "/services/custom-website-design-and-development",
+        destination:
+          "/services/custom-website-design-and-development-that-powers-your-business",
+        permanent: true
+      },
+      {
         source: "/services/app-store-distribution-services",
         destination:
-          "/app-store-distribution-services-that-get-your-app-approved-fast",
+          "/services/app-store-distribution-services-that-get-your-app-approved-fast",
         permanent: true
       }
     ];
