f97f70f70e6c1b60a5fd151d8e1e146a37debf17
topher <topherjamesknoll@gmail.com>
2025-11-22 20:12:59 -0800

    Think twice. Code once.

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,28 @@ const nextConfig: NextConfig = {
     remotePatterns: [new URL("https://cms.bessaapps.com/**")]
   },
   async redirects() {
-    return [];
+    return [
+      {
+        source: "/services/app-store-distribution-services",
+        destination: "/services/app-store-distribution",
+        permanent: true
+      },
+      {
+        source: "/services/custom-mobile-app-development",
+        destination: "/services/mobile",
+        permanent: true
+      },
+      {
+        source: "/services/custom-website-design-and-development",
+        destination: "/services/web",
+        permanent: true
+      },
+      {
+        source: "/services/custom-seo-and-aso-solutions-that-drive-discovery",
+        destination: "/services/aso-and-seo",
+        permanent: true
+      }
+    ];
   }
 };
 
