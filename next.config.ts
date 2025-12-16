312dbc1101bf7225c96c5ed308eb7da5dde737fe
topher <topherjamesknoll@gmail.com>
2025-12-16 10:56:42 -0800

    Think twice. Code once.

--- next.config.ts
+++ next.config.ts
@@ -7,43 +7,32 @@ const nextConfig: NextConfig = {
   async redirects() {
     return [
       {
-        source: "/articles/:path*",
-        destination: "/launchpad/:path*",
+        source: "/services/custom-mobile-app-development",
+        destination: "/services/custom-mobile-app-development-for-your-vision",
         permanent: true
       },
       {
-        source: "/services/custom-mobile-app-development-that-drives-growth",
-        destination: "/services/custom-mobile-app-development",
+        source: "/services/website-development-services-that-drive-results",
+        destination: "/services/custom-mobile-app-development-for-your-vision",
         permanent: true
       },
       {
-        source: "/services/web",
-        destination:
+        source:
           "/services/custom-website-design-and-development-that-powers-your-business",
+        destination: "/services/custom-mobile-app-development-for-your-vision",
         permanent: true
       },
       {
-        source: "/services/app-store-distribution",
-        destination:
+        source:
           "/services/stress-free-app-store-submission-services-that-get-you-approved-fast",
+        destination: "/services/app-store-submission-services-for-ios-android",
         permanent: true
       },
       {
-        source: "/services/app-store-distribution-services",
-        destination:
-          "/services/stress-free-app-store-submission-services-that-get-you-approved-fast",
-        permanent: true
-      },
-      {
-        source: "/services/aso-and-seo",
-        destination:
+        source:
           "/services/smarter-aso-and-seo-services-for-mobile-apps-to-increase-rankings-and-conversions",
-        permanent: true
-      },
-      {
-        source: "/services/aso-and-seo",
         destination:
-          "/services/smarter-aso-and-seo-services-for-mobile-apps-to-increase-rankings-and-conversions",
+          "/services/seo-and-aso-services-that-boost-visibility-and-downloads",
         permanent: true
       }
     ];
