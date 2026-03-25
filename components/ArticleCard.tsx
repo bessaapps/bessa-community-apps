8bd1a52fba40251dfdbb77b74cb6b0fd075ae363
topher <topherjamesknoll@gmail.com>
2026-03-25 11:35:37 -0700

    Think twice. Code once.

--- components/ArticleCard.tsx
+++ components/ArticleCard.tsx
@@ -20,7 +20,8 @@ export default function ArticleCard({ article }: { article: Post }) {
           src={article._embedded["wp:featuredmedia"][0].source_url}
           alt={formatTitle(title)}
           fill
-          objectFit={"cover"}
+          style={{ objectFit: "cover" }}
+          sizes={"640px"}
         />
         <div
           className={"absolute inset-0 bg-linear-to-b from-transparent to-card"}
