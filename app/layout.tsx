139e2c3dac85efe6bedf40f142114b7a7bb2629f
topher <topherjamesknoll@gmail.com>
2026-05-19 11:38:41 -0700

    Commented out Google Analytics integration in layout file.

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,6 +40,7 @@ export default function RootLayout({
         <Clock />
         <Weather />
       </body>
+
       {/*{process.env.NODE_ENV !== "development" && (*/}
       {/*  <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID!} />*/}
       {/*)}*/}
