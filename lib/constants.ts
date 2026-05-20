34919e5aedb472808425c062bbc6db146e7cf0c8
topher <topherjamesknoll@gmail.com>
2026-05-20 16:56:43 -0700

    Updated app constants: revised keyword and description for broader application development focus.

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,7 @@
-export const keyword = "Custom Mobile App Development Services";
+export const keyword = "Custom Application Development";
 export const title = "Bessa Community Apps";
 export const url = "https://bessaapps.com";
 export const description =
-  "Elevate your business with my custom mobile app development services. Tailored solutions to meet your unique needs and enhance user engagement.";
+  "Stop dreaming, start launching with my custom application development. I guide you end-to-end, from concept to app store for iOS, Android, and web.";
 
 export const bookingLink = "https://calendar.app.google/uGBwb7WfWXLUgSU6A";
