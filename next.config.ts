eb592e88c2dae72dcde6f39fe31579093140f99c
topher <topherjamesknoll@gmail.com>
2026-05-23 18:17:26 -0700

    Removed outdated redirects from next.config.ts and enabled "export" output mode.

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   images: {
     remotePatterns: [new URL("https://cms.bessaapps.com/**")]
   },
   async redirects() {
-    return [
-      {
-        source: "/services/custom-mobile-app-development-for-your-vision",
-        destination: "/mobile-app-development",
-        permanent: true
-      },
-      {
-        source: "/services/app-store-submission-services-for-ios-android",
-        destination: "/app-store-submission",
-        permanent: true
-      },
-      {
-        source:
-          "/services/seo-and-aso-services-that-boost-visibility-and-downloads",
-        destination: "/app-store-optimization",
-        permanent: true
-      }
-    ];
+    return [];
   }
 };
 
