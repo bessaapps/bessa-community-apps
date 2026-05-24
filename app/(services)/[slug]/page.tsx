52aae1ccf51cc537c0bcd9e1a892eb054b32869a
topher <topherjamesknoll@gmail.com>
2026-05-23 21:48:24 -0700

    Commented out unused `Process` component in Services page to clean up the UI.

--- app/(services)/[slug]/page.tsx
+++ app/(services)/[slug]/page.tsx
@@ -119,7 +119,7 @@ export default async function ServicePage({
         />
       </div>
       <ServicesSection sectionHeading={"More Services"} hiddenId={post.id} />
-      <Process />
+      {/*<Process />*/}
     </section>
   );
 }
