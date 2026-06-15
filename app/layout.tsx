ca917856fa3856cfc44c00f0337117ceeb68afa4
topher <topherjamesknoll@gmail.com>
2026-06-15 16:06:08 -0700

    Removed Ahrefs script from `layout.tsx` to simplify analytics configuration.

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,14 +41,7 @@ export default function RootLayout({
         <Weather />
       </body>
       {process.env.NODE_ENV !== "development" && (
-        <>
-          <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID!} />
-          <script
-            src={"https://analytics.ahrefs.com/analytics.js"}
-            data-key={"3mOIblmw6vrADXOMbB0XGg"}
-            async
-          />
-        </>
+        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID!} />
       )}
     </html>
   );
