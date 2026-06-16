666b717ef9973f4b53bbee2bfbe08b75b04a7f7d
topher <topherjamesknoll@gmail.com>
2026-06-16 13:40:39 -0700

    Updated `[slug]/page.tsx` to use `meta_title` from ACF for metadata and share buttons fallback.

--- app/launchpad/[slug]/page.tsx
+++ app/launchpad/[slug]/page.tsx
@@ -31,7 +31,7 @@ export async function generateMetadata({
   const excerpt = stripHtml(post.excerpt.rendered).result;
 
   return formatMetadata({
-    metadataTitle: title,
+    metadataTitle: post?.acf?.meta_title || title,
     metadataDescription: excerpt,
     path: `/launchpad/${post.slug}`,
     imagePath: post._embedded["wp:featuredmedia"][0].source_url
@@ -59,7 +59,7 @@ export default async function ArticlePage({
   const jsonLd: WithContext<BlogPosting> = {
     "@context": "https://schema.org",
     "@type": "BlogPosting",
-    headline: title,
+    headline: post?.acf?.meta_title || title,
     image: post._embedded["wp:featuredmedia"][0].source_url,
     description: excerpt,
     publisher: {
@@ -155,7 +155,7 @@ export default async function ArticlePage({
             <div className={"flex justify-end"}>
               <ShareButtons
                 url={`${url}/launchpad/${post.slug}`}
-                title={post.title.rendered}
+                title={post?.acf?.meta_title || title}
               />
             </div>
           </div>
