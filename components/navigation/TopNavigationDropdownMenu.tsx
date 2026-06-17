2ebcb4f7357797b2067d70402a17bb56ca5924c9
topher <topherjamesknoll@gmail.com>
2026-06-16 22:55:10 -0700

    Removed `DropdownMenu` component and related modules; upgraded `radix-ui` dependencies to latest versions for compatibility.

--- components/navigation/TopNavigationDropdownMenu.tsx
+++ components/navigation/TopNavigationDropdownMenu.tsx
@@ -1,30 +1,35 @@
 "use client";
 
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger
-} from "@/components/ui/dropdown-menu";
 import { AiOutlineMenu } from "react-icons/ai";
 import Link from "next/link";
 import { LINKS } from "@/lib/constants";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { useState } from "react";
 
 export default function TopNavigationDropdownMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild className={"cursor-pointer"}>
+    <Sheet open={isOpen}>
+      <SheetTrigger onClick={() => setIsOpen(true)}>
         <AiOutlineMenu size={24} />
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align={"end"}>
+      </SheetTrigger>
+      <SheetContent className={"p-4"} onBlur={() => setIsOpen(false)}>
         <nav>
-          {LINKS.map(({ href, anchor }) => (
-            <Link key={href} href={href} title={anchor}>
-              <DropdownMenuItem>{anchor}</DropdownMenuItem>
-            </Link>
-          ))}
+          <ul>
+            {LINKS.map(({ href, anchor }) => (
+              <Link
+                key={href}
+                href={href}
+                title={anchor}
+                onClick={() => setIsOpen(false)}
+              >
+                <li className={"text-primary font-medium"}>{anchor}</li>
+              </Link>
+            ))}
+          </ul>
         </nav>
-      </DropdownMenuContent>
-    </DropdownMenu>
+      </SheetContent>
+    </Sheet>
   );
 }
