d6a6ba1492f744db4412fd4c513af56c8dd4d5d7
topher <topherjamesknoll@gmail.com>
2026-06-22 22:36:47 -0700

    Added process stage images to `ProcessSection`, updated layout for improved structure, and added new assets for each stage.

--- components/ProcessSection.tsx
+++ components/ProcessSection.tsx
@@ -1,55 +1,71 @@
+import Discovery from "@/assets/images/process/discovery.png";
+import Design from "@/assets/images/process/design.png";
+import Build from "@/assets/images/process/build.png";
+import QualityAssurance from "@/assets/images/process/quality-assurance.png";
+import Distribution from "@/assets/images/process/distribution.png";
+import Marketing from "@/assets/images/process/marketing.png";
+import Maintenance from "@/assets/images/process/maintenance.png";
+import Image from "next/image";
+
 export default function ProcessSection() {
   const processes = [
     {
       heading: "Discovery",
       text: "This is where every great app begins. This stage includes open discussion to understand your goals, in-depth research to identify opportunities, and clear scope definition to set the foundation for success. By aligning vision and strategy from the start, this phase ensures that development moves forward with focus, efficiency, and confidence.",
-      steps: ["Discuss", "Research", "Define Scope"]
+      image: Discovery
     },
     {
       heading: "Design",
       text: "This will bring your app to life visually with a clear style guide and prototypes. This stage defines the look, feel, and flow of your product, ensuring consistency across every screen and a seamless user experience. By combining creativity with usability, this phase lays the groundwork for an app that is both visually stunning and intuitive to navigate.",
-      steps: ["Create a Style Guide", "Prototype Designs"]
+      image: Design
     },
     {
       heading: "Build",
       text: "Here Ideas will take shape and come to life. During this stage, layouts are crafted for clarity and usability, functionality is built to deliver seamless performance, and content is integrated to create a polished, engaging experience. Every detail is handled with precision to ensure the final product reflects your vision and meets the highest standards of quality.",
-      steps: [
-        "Layout Components",
-        "Install Functionality",
-        "Incorporate Content"
-      ]
+      image: Build
+    },
+    {
+      heading: "Quality Assurance",
+      text: "Here Ideas will take shape and come to life. During this stage, layouts are crafted for clarity and usability, functionality is built to deliver seamless performance, and content is integrated to create a polished, engaging experience. Every detail is handled with precision to ensure the final product reflects your vision and meets the highest standards of quality.",
+      image: QualityAssurance
     },
     {
       heading: "Distribution",
       text: "The final state ensures your app reaches the world with impact. This stage includes preparing your app for launch and managing submission to app stores, handling requirements and guidelines with expertise. By streamlining the process, the distribution phase makes your app accessible to users quickly and positions it for maximum visibility and success.",
-      steps: [
-        "Gather Requirements",
-        "Submit for Review",
-        "Publish to App Stores"
-      ]
+      image: Distribution
+    },
+    {
+      heading: "Marketing",
+      text: "The final state ensures your app reaches the world with impact. This stage includes preparing your app for launch and managing submission to app stores, handling requirements and guidelines with expertise. By streamlining the process, the distribution phase makes your app accessible to users quickly and positions it for maximum visibility and success.",
+      image: Marketing
+    },
+    {
+      heading: "Maintenance",
+      text: "The final state ensures your app reaches the world with impact. This stage includes preparing your app for launch and managing submission to app stores, handling requirements and guidelines with expertise. By streamlining the process, the distribution phase makes your app accessible to users quickly and positions it for maximum visibility and success.",
+      image: Maintenance
     }
   ];
 
+  return null;
+
   return processes?.map((process, index) => (
     <div key={index} className={"py-8"}>
-      <div className={"grid sm:grid-cols-2 gap-4 mb-8"}>
-        <div>
-          <div className={"flex items-center gap-4"}>
-            <h3 className={"text-xl font-bold"}>{process.heading}</h3>
-          </div>
+      <div className={"grid grid-cols-4 gap-4"}>
+        <div
+          className={
+            "w-full max-w-full h-full max-h-full aspect-square flex items-center justify-center"
+          }
+        >
+          <Image
+            src={process.image}
+            alt={process.heading}
+            style={{ height: "100%", width: "100%", objectFit: "contain" }}
+          />
         </div>
-        <div className={"flex flex-col gap-6"}>
-          <p>{process.text}</p>
-          <p className={"text-center"}>&darr;</p>
-          <div className={"flex flex-col gap-2"}>
-            {process.steps.map((step) => (
-              <p
-                key={step}
-                className={"text-primary font-semibold text-center"}
-              >
-                {step}
-              </p>
-            ))}
+        <div className={"col-span-3 flex items-center"}>
+          <div className={"p-4"}>
+            <h3 className={"text-xl font-bold mb-4"}>{process.heading}</h3>
+            <p>{process.text}</p>
           </div>
         </div>
       </div>
