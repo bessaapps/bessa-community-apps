665bf018825e14f05cb90ffa2db98d21cbfcab65
topher <topherjamesknoll@gmail.com>
2026-06-23 14:51:11 -0700

    Updated `sizes` attribute for process images in `ProcessSection` to improve responsiveness on smaller screens.

--- components/ProcessSection.tsx
+++ components/ProcessSection.tsx
@@ -80,6 +80,7 @@ export default function ProcessSection() {
                     src={process.image}
                     alt={process.heading}
                     className={"h-full w-full object-contain"}
+                    sizes={"(max-width: 640px) 241px, 74px"}
                   />
                 </div>
                 <div className={"col-span-3 flex items-center"}>
