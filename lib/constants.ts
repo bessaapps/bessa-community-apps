17116e953a36543b2b873a5d3987efa0b07ecc13
topher <topherjamesknoll@gmail.com>
2026-06-30 13:15:21 -0700

    Updated `FAQS` entries in `constants.ts` to include revised content and new questions; streamlined outdated information.

--- lib/constants.ts
+++ lib/constants.ts
@@ -17,16 +17,14 @@ export const SOCIALS = [
 ];
 
 export const FAQS = {
-  "What does your mobile development and consulting process look like?":
-    "Your app idea is great. Now what? Many individuals and organizations initially feel immobilized. They have a mobile app concept but struggle with how to transform it into a truly impactful product. My mobile development and consulting approach offers an end-to-end process, from ideation to app distribution. I specialize in guiding innovators, organizations, and communities from initial ideation all the way through to launch. Best of all, I make the mobile app development process feel non-intimidating.",
-  "Do you offer local mobile development and consulting for businesses in Las Vegas, Nevada?":
-    "Yes! While I am based in Las Vegas, Nevada, I provide mobile development and consulting for partners nationwide. I work primarily with three groups of people: innovators (entrepreneurs) for building and scaling digital solutions; organizations (non-profits, businesses, and education) for internal/external customizable app solutions; and communities for fostering connection, coordinating events, and collective action. No matter where you are, I expand clients’ digital presence with cross-platform apps for iOS, Android, and the web.",
-  "How much does mobile development and consulting cost?":
-    "To keep things transparent for clients in the ideation and concept generation stage, my hourly rate is $50.",
-  "Why should I choose Bessa Community Apps for mobile development and consulting?":
-    "Stop dreaming, start launching. I understand that uncertainty, coupled with a lack of familiarity with mobile development and app distribution, often makes people hesitant to proceed. I stand out in mobile development and consulting because I'm both creative and logical, and I keep my services empathetic and inclusive. I don't just build software; I create apps that work beautifully across iOS, Android, and the web, and are built to grow.",
-  "Do you have resources for those just starting to explore mobile development and consulting?":
-    'Absolutely. For clients seeking mobile development and consulting advice, I host a blog named Launchpad. The blog focuses on moving clients past the feeling of being "immobilized". It is specifically designed for innovators and organizations in the ideation and concept generation stage who need a clear path forward.',
-  "What makes your Las Vegas-based mobile development and consulting services different for someone who has never built an app before?":
-    "If you are located here in Las Vegas, Nevada, or anywhere else, I know that having a great idea is only the beginning. Many individuals and organizations I work with initially feel immobilized. They have a mobile app concept but struggle with how to transform it into a truly impactful product. My mobile development and consulting services are distinct because I make the mobile app development process feel non-intimidating. Built on values of innovation, curiosity, and creativity, I offer an end-to-end process, from ideation to app distribution. By serving as a solution-oriented partner, my goal is to smoothly guide you from concept to app store."
+  "What Cross-Platform App Solutions Do You Offer?":
+    "I see your project through from your first idea to publishing it on the app stores. We’ll validate your idea to make sure it’s something people want, scope out a plan for success, create a beautiful and accessible designs, build functionality that solves your users’ pain points, test your app to ensure it offers value, distribute your app online and in the app stores to connect with your audience, further market your app to see it grow, and keep it up-to-date to keep it valuable even over time.",
+  "What is React Native?":
+    "React Native and Expo are the tools we'll use to build your mobile app for iOS, Android, and the web simultaneously. Not only will we transform your brilliant idea into a high-demand app, but we’ll also make it readily accessible on mobile and web, and available on the Apple App Store, Google Play, and others, getting your app into the hands of more people.",
+  "What are the Apple App Store and Google Play?":
+    "Apple, Google, and other companies like Amazon have their own app stores where people can download iOS, Android, and APK apps. Apple has the App Store, and Google has Google Play. Let’s make sure yours is available on both. Not only will we build your app for web and mobile, but we’ll also submit and distribute it to the internet, the Apple App Store, Google Play, and others.",
+  "How Much Do Your App Development Services Cost?":
+    "My rate is set to a modest $50/hr, meaning projects typically start around $15,000, depending on the scope of work.",
+  "Will You Sign a Non-Disclosure Agreement (NDA)?":
+    "Of course! I’m excited to hear your idea and turn it into reality, but it’s your idea; my goal is to publish your idea to the app stores, not steal it from you. If you like, I have an NDA already drawn up for you on my Contact page. You can easily fill it out and sign it online, and I’ll sign it and send it back!"
 };
