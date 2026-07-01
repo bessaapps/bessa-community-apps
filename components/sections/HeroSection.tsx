0d8d928144457cfbeb2d6bcfd65f5da1ccaf6605
topher <topherjamesknoll@gmail.com>
2026-06-30 19:16:37 -0700

    Updated `HeroSection` layout: improved responsive typography and refactored call-to-action for better alignment and flexibility.

--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -28,7 +28,11 @@ export default function HeroSection() {
         }
       >
         <div className={"flex flex-col gap-4"}>
-          <h1 className={"text-5xl font-bold leading-[1.1] max-w-[575]"}>
+          <h1
+            className={
+              "text-4xl sm:text-5xl font-bold leading-[1.1] max-w-[575]"
+            }
+          >
             From Impactful Concept to App Store Launch
           </h1>
           <p className={"text-xl max-w-[575] mb-8"}>
@@ -36,49 +40,49 @@ export default function HeroSection() {
             organizations, and communities, and publishing them to the Apple App
             Store and Google Play.
           </p>
-          <div className={"flex items-center gap-4"}>
-            <div>
-              <Link href={"#services"} title={"Services"}>
-                <Button className={"cursor-pointer hover:scale-110"}>
-                  Learn More
-                </Button>
-              </Link>
-            </div>
-            <div className={"relative h-f aspect-square w-10"}>
-              <div
-                className={
-                  "h-full w-full relative rounded-full overflow-hidden"
-                }
-              >
-                <Image
-                  src={Me}
-                  alt={
-                    "Headshot of a smiling developer with glasses and a mustache, an expert providing custom mobile app development services."
+          <div className={"flex flex-col sm:flex-row sm:items-center gap-4"}>
+            <Link href={"#services"} title={"Services"}>
+              <Button className={"cursor-pointer hover:scale-110"}>
+                Learn More
+              </Button>
+            </Link>
+            <div className={"flex items-center gap-4"}>
+              <div className={"relative h-f aspect-square w-10"}>
+                <div
+                  className={
+                    "h-full w-full relative rounded-full overflow-hidden"
                   }
-                  className={"object-cover"}
-                  fill
-                />
-              </div>
-              <div
-                className={
-                  "bg-green-700 h-4 w-4 absolute bottom-0 right-0 flex items-center justify-center rounded-full translate-x-1/4 translate-y-1/4"
-                }
-              >
-                <Zap color={"white"} size={12} />
-              </div>
-            </div>
-            <p className={"text-primary text-xs max-w-[160]"}>
-              Free discovery call to clarify your goals{" "}
-              <span className={"font-bold"}>
-                <Link
-                  href={bookingLink}
-                  target={"_blank"}
-                  rel={"noopener noreferrer"}
                 >
-                  Schedule now
-                </Link>
-              </span>
-            </p>
+                  <Image
+                    src={Me}
+                    alt={
+                      "Headshot of a smiling developer with glasses and a mustache, an expert providing custom mobile app development services."
+                    }
+                    className={"object-cover"}
+                    fill
+                  />
+                </div>
+                <div
+                  className={
+                    "bg-green-700 h-4 w-4 absolute bottom-0 right-0 flex items-center justify-center rounded-full translate-x-1/4 translate-y-1/4"
+                  }
+                >
+                  <Zap color={"white"} size={12} />
+                </div>
+              </div>
+              <p className={"text-primary text-xs max-w-[160]"}>
+                Free discovery call to clarify your goals{" "}
+                <span className={"font-bold"}>
+                  <Link
+                    href={bookingLink}
+                    target={"_blank"}
+                    rel={"noopener noreferrer"}
+                  >
+                    Schedule now
+                  </Link>
+                </span>
+              </p>
+            </div>
           </div>
         </div>
       </div>
